3a2ca325dbae11178cc8ea2e552b268f7c15a220
Lian Negrão <lian.negrao@ba.estudante.senai.br>
2025-11-07 11:46:23 -0300

    feat: Add sidebar navigation with toggle functionality to dashboard and home components

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+menuAberto = false;
 
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  irParaDashboard() {
+    this.menuAberto = false;
+    this.router.navigate(['/dashboard']);
+  }
+
+  irParaHome() {
+    this.menuAberto = false;
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ menuAberto = false;
 
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  irParaDashboard() {
+    this.menuAberto = false;
+    this.router.navigate(['/dashboard']);
+  }
+
+  irParaHome() {
+    this.menuAberto = false;
+    this.router.navigate(['/home']);
+  }
 }
